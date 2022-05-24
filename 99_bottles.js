f66ecfd9f3fe3fda6e715b4268c9651b6d0157e7
kineticvenom <kalebvarnes@yahoo.com>
2022-05-24 01:37:51 -1000

    Updated code

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,23 @@
 function bottleSong() {
-  // Write your code here!
+  let numOfBeer = 99;
+
+  for (let i = 0; i < 99; i++) {
+    if (numOfBeer > 2) {
+      console.log(numOfBeer + " bottles of beer on the wall, " + numOfBeer + " bottles of beer.");
+      numOfBeer--;
+      console.log("Take one down and pass it around, " + numOfBeer + " bottles of beer on the wall.");
+    }
+    if (numOfBeer <= 2 && numOfBeer !== 0) {
+      console.log(numOfBeer + " bottles of beer on the wall, " + numOfBeer + " bottles of beer.");
+      numOfBeer--;
+      console.log("Take one down and pass it around, " + numOfBeer + " bottle of beer.");
+
+      console.log(numOfBeer + " bottle of beer on the wall, " + numOfBeer + " bottle of beer.")
+      console.log("Take one down and pass it around, no more bottles of beer.");
+      numOfBeer--;
+      console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
+    };
+  };
 };
 
 bottleSong();
