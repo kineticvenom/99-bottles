3052dd533fd267b20df82894201db31952099409
kineticvenom <kalebvarnes@yahoo.com>
2022-05-25 10:37:59 -1000

    Python code with loop

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,14 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+	bottles_of_beer = num
+	
+	while bottles_of_beer > 1: 
+		print(f"{bottles_of_beer} bottles of beer on the wall, {bottles_of_beer} bottles of beer.\nTake one down and pass it around, {bottles_of_beer - 1} bottles of beer on the wall.")
+		bottles_of_beer -= 1 
+	
+	if num == 1:
+		print(f"{bottles_of_beer} bottle of beer on the wall.\n{bottles_of_beer} bottle of beer on the wall, {bottles_of_beer} bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+ 	
 
+	
 
-bottle_song()
+bottle_song(10)
