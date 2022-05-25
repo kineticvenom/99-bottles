aba0ed61c6e2238fef7665cc98081b90f5d7b4ea
kineticvenom <kalebvarnes@yahoo.com>
2022-05-25 11:44:15 -1000

    update

--- 99_bottles.py
+++ 99_bottles.py
@@ -7,8 +7,6 @@ def bottle_song(num):
 	
 	if num == 1:
 		print(f"{bottles_of_beer} bottle of beer on the wall.\n{bottles_of_beer} bottle of beer on the wall, {bottles_of_beer} bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
- 	
-
-	
 
 bottle_song(10)
+
